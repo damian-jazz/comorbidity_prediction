130301e857c1d780dfb0cc91605447affb5cce67
Damian Jaspar <damianjaspar@mailbox.org>
2024-03-17 20:32:48 +0100

    Added train and test for focal loss + refactoring

--- utils/cnn_train.py
+++ utils/cnn_train.py
@@ -1,76 +1,9 @@
 import torch
+from cnn_utils import sigmoid_focal_loss
 
-def train(dataloader, device, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
+##### bce loss #####
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
-        
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if (batch % 1 == 0):
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-def test(dataloader, device, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    
-    model.eval()
-    test_loss, correct = 0, 0
-    
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            
-            test_loss += loss_fn(pred, y).item()
-
-            pred[pred < 0.5] = 0
-            pred[pred >= 0.5] = 1
-
-            correct += (pred == y).type(torch.float).sum().item()
-            
-    test_loss /= num_batches
-    correct /= dataloader.dataset.labels.shape[1] # to account for multiple labels
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-def eval(dataloader, device, model, loss_fn):
-    y_prob = []
-    y_pred = []
-
-    model.eval()
-    with torch.no_grad():
-        for batch, (X, _) in enumerate(dataloader):
-            X = X.to(device)
-            out = model(X)
-
-            prob = torch.sigmoid(out).detach().cpu()  
-            pred = torch.sigmoid((out)).detach().cpu()
-            pred[pred >= 0.5] = 1
-            pred[pred < 0.5] = 0
-
-            if batch == 0:
-                y_prob = prob
-                y_pred = pred
-            else:
-                y_prob = torch.cat((y_prob, prob), 0)
-                y_pred = torch.cat((y_pred, pred), 0)
-
-    return y_prob, y_pred
-
-
-########## with logging ##########
-
-def train_with_logging(dataloader, device, model, loss_fn, optimizer, logging, period=20):
+def train(dataloader, device, model, loss_fn, optimizer, logging, period=20):
     size = len(dataloader.dataset)
     model.train()
     for batch, (X, y) in enumerate(dataloader):
@@ -91,7 +24,7 @@ def train_with_logging(dataloader, device, model, loss_fn, optimizer, logging, p
     
     logging.info(f"\tTrain loss: {loss:.4f}") 
 
-def test_with_logging(dataloader, device, model, loss_fn, logging):
+def test(dataloader, device, model, loss_fn, logging):
     size = len(dataloader.dataset)
     num_batches = len(dataloader)
     
@@ -115,3 +48,77 @@ def test_with_logging(dataloader, device, model, loss_fn, logging):
     correct /= size
     logging.info(f"\tTest accuracy: {(100*correct):>0.1f}")
     logging.info(f"\tTest avg loss: {test_loss:>4f}")
+
+##### focal loss #####
+
+def train_focal(dataloader, device, model, optimizer, logging, period=20):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = sigmoid_focal_loss(pred, y)
+        
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if (batch % period == 0):
+            loss, current = loss.item(), (batch + 1) * len(X)
+            logging.info(f"\tTrain loss: {loss:.4f}  [{current:>5d}/{size:>5d}]") 
+
+    logging.info(f"\tTrain loss: {loss:.4f}") 
+
+def test_focal(dataloader, device, model, logging):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+
+    model.eval()
+    test_loss, correct = 0, 0
+
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            
+            test_loss += sigmoid_focal_loss(pred, y).item()
+
+            pred[pred < 0.5] = 0
+            pred[pred >= 0.5] = 1
+
+            correct += (pred == y).type(torch.float).sum().item()
+            
+    test_loss /= num_batches
+    correct /= dataloader.dataset.labels.shape[1] # to account for multiple labels
+    correct /= size
+    logging.info(f"\tTest accuracy: {(100*correct):>0.1f}")
+    logging.info(f"\tTest avg loss: {test_loss:>4f}")
+
+##### eval #####
+
+def eval(dataloader, device, model):
+    y_prob = []
+    y_pred = []
+
+    model.eval()
+    with torch.no_grad():
+        for batch, (X, _) in enumerate(dataloader):
+            X = X.to(device)
+            out = model(X)
+
+            prob = torch.sigmoid(out).detach().cpu()  
+            pred = torch.sigmoid((out)).detach().cpu()
+            pred[pred >= 0.5] = 1
+            pred[pred < 0.5] = 0
+
+            if batch == 0:
+                y_prob = prob
+                y_pred = pred
+            else:
+                y_prob = torch.cat((y_prob, prob), 0)
+                y_pred = torch.cat((y_pred, pred), 0)
+
+    return y_prob, y_pred
